48c8d75df070fcb513f3cd0034e9886a2feb6421
ElsoMichael <elsomichael@ymail.com>
2018-04-12 10:13:43 -0400

    still working on react stuff

---
+++ stardewDatabase/routes/api.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var router = express.Router();
+const Animals = require('../models/animals');
+const Artifacts = require('../models/artifacts');
+const Crops = require('../models/crops');
+const Fishing = require('../models/fishing');
+const Villagers = require('../models/villagers');
+
+module.exports = router;
